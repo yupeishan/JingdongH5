20643cc9efa1a3434fdb8b57bb27abdf57ae0e1c
yupeishan <104750701+yupeishan@users.noreply.github.com>
2022-11-28 14:33:34 +0800

    pull commit

--- src/main/java/com/example/jingdong/constant/Const.java
+++ src/main/java/com/example/jingdong/constant/Const.java
@@ -6,4 +6,5 @@ public class Const {
     public static final String START_TIME= "1970-01-01";
     //订单搜索查询 结束时间  timeStamp类型取值范围max
     public static final String END_TIME= "2038-01-19";
+     public static final String test= "2038-01-19";
 }
